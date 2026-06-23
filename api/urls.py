093b46681ab454573150a3cce62365709845778a
Og'abek <ogabekazamov06@gmail.com>
2026-06-23 22:50:49 +0500

    7-dars tugadi

--- api/urls.py
+++ api/urls.py
@@ -1,17 +1,31 @@
-from django.urls import path
-from .views import (
-    CarBrandListCreateView, CarBrandRetrieveUpdateDestroyView,
-    CarListCreateView, CarRetrieveUpdateDestroyView,
-    CommentListCreateView, CommentRetrieveUpdateDestroyView
-)
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+from .views import CarBrandViewSet, CarViewSet, CommentViewSet
+
+router = DefaultRouter()
+router.register(r'brands', CarBrandViewSet, basename='brand')
+router.register(r'cars', CarViewSet, basename='car')
+router.register(r'comments', CommentViewSet, basename='comment')
 
 urlpatterns = [
-    path('api/brands/', CarBrandListCreateView.as_view(), name='brand_list_create'),
-    path('api/brands/<int:pk>/', CarBrandRetrieveUpdateDestroyView.as_view(), name='brand_detail'),
-
-    path('api/cars/', CarListCreateView.as_view(), name='car_list_create'),
-    path('api/cars/<int:pk>/', CarRetrieveUpdateDestroyView.as_view(), name='car_detail'),
-
-    path('api/comments/', CommentListCreateView.as_view(), name='comment_list_create'),
-    path('api/comments/<int:pk>/', CommentRetrieveUpdateDestroyView.as_view(), name='comment_detail'),
+    path('api/', include(router.urls)),
+    path('api/auth/token/', obtain_auth_token, name='obtain_token'),
 ]
+# from django.urls import path
+# # from .views import (
+# #     CarBrandListCreateView, CarBrandRetrieveUpdateDestroyView,
+# #     CarListCreateView, CarRetrieveUpdateDestroyView,
+# #     CommentListCreateView, CommentRetrieveUpdateDestroyView
+# # )
+#
+# urlpatterns = [
+#     path('api/brands/', CarBrandListCreateView.as_view(), name='brand_list_create'),
+#     path('api/brands/<int:pk>/', CarBrandRetrieveUpdateDestroyView.as_view(), name='brand_detail'),
+#     path('api/cars/', CarListCreateView.as_view(), name='car_list_create'),
+#     path('api/cars/<int:pk>/', CarRetrieveUpdateDestroyView.as_view(), name='car_detail'),
+#
+#     path('api/comments/', CommentListCreateView.as_view(), name='comment_list_create'),
+#     path('api/comments/<int:pk>/', CommentRetrieveUpdateDestroyView.as_view(), name='comment_detail'),
+# ]
--- api/views.py
+++ api/views.py
@@ -1,45 +1,15 @@
-from rest_framework import generics
-from .models import CarBrand, Car,Comment
+from rest_framework import viewsets
+from .models import CarBrand, Car, Comment
 from .serializers import CarBrandSerializer, CarSerializer, CommentSerializer
 from .permissions import IsAdminOrReadOnly, IsAuthorOrAdminOrReadOnly
 
 
-class CarBrandListCreateView(generics.ListCreateAPIView):
+class CarBrandViewSet(viewsets.ModelViewSet):
     queryset = CarBrand.objects.all()
     serializer_class = CarBrandSerializer
     permission_classes = [IsAdminOrReadOnly]
 
-
-class CarBrandRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = CarBrand.objects.all()
-    serializer_class = CarBrandSerializer
-    permission_classes = [IsAdminOrReadOnly]
-
-class CarListCreateView(generics.ListCreateAPIView):
-    serializer_class = CarSerializer
-    permission_classes = [IsAdminOrReadOnly]
-
-    def get_queryset(self):
-        queryset = Car.objects.all()
-        brand_id = self.request.query_params.get('brand_id')
-        min_price = self.request.query_params.get('min_price')
-        max_price = self.request.query_params.get('max_price')
-        if brand_id is not None:
-            queryset = queryset.filter(brand_id=brand_id)
-        if min_price is not None:
-            queryset = queryset.filter(price__gte=min_price)
-        if max_price is not None:
-            queryset = queryset.filter(price__lte=max_price)
-        return queryset
-
-
-class CarRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Car.objects.all()
-    serializer_class = CarSerializer
-    permission_classes = [IsAdminOrReadOnly]
-
-
-class CarListCreateView(generics.ListCreateAPIView):
+class CarViewSet(viewsets.ModelViewSet):
     serializer_class = CarSerializer
     permission_classes = [IsAdminOrReadOnly]
 
@@ -55,18 +25,116 @@ class CarListCreateView(generics.ListCreateAPIView):
         return queryset
 
 
-class CarRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Car.objects.all()
-    serializer_class = CarSerializer
-    permission_classes = [IsAdminOrReadOnly]
 
-class CommentListCreateView(generics.ListCreateAPIView):
+class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [IsAuthorOrAdminOrReadOnly]
 
 
-class CommentRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-    permission_classes = [IsAuthorOrAdminOrReadOnly]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from rest_framework import generics
+# from .models import CarBrand, Car,Comment
+# from .serializers import CarBrandSerializer, CarSerializer, CommentSerializer
+# from .permissions import IsAdminOrReadOnly, IsAuthorOrAdminOrReadOnly
+#
+#
+# class CarBrandListCreateView(generics.ListCreateAPIView):
+#     queryset = CarBrand.objects.all()
+#     serializer_class = CarBrandSerializer
+#     permission_classes = [IsAdminOrReadOnly]
+#
+#
+# class CarBrandRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = CarBrand.objects.all()
+#     serializer_class = CarBrandSerializer
+#     permission_classes = [IsAdminOrReadOnly]
+#
+# class CarListCreateView(generics.ListCreateAPIView):
+#     serializer_class = CarSerializer
+#     permission_classes = [IsAdminOrReadOnly]
+#
+#     def get_queryset(self):
+#         queryset = Car.objects.all()
+#         brand_id = self.request.query_params.get('brand_id')
+#         min_price = self.request.query_params.get('min_price')
+#         max_price = self.request.query_params.get('max_price')
+#         if brand_id is not None:
+#             queryset = queryset.filter(brand_id=brand_id)
+#         if min_price is not None:
+#             queryset = queryset.filter(price__gte=min_price)
+#         if max_price is not None:
+#             queryset = queryset.filter(price__lte=max_price)
+#         return queryset
+#
+#
+# class CarRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Car.objects.all()
+#     serializer_class = CarSerializer
+#     permission_classes = [IsAdminOrReadOnly]
+#
+#
+# class CarListCreateView(generics.ListCreateAPIView):
+#     serializer_class = CarSerializer
+#     permission_classes = [IsAdminOrReadOnly]
+#
+#     def get_queryset(self):
+#         queryset = Car.objects.all()
+#         brand_id = self.request.query_params.get('brand_id')
+#         min_price = self.request.query_params.get('min_price')
+#         max_price = self.request.query_params.get('max_price')
+#
+#         if brand_id: queryset = queryset.filter(brand_id=brand_id)
+#         if min_price: queryset = queryset.filter(price__gte=min_price)
+#         if max_price: queryset = queryset.filter(price__lte=max_price)
+#         return queryset
+#
+#
+# class CarRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Car.objects.all()
+#     serializer_class = CarSerializer
+#     permission_classes = [IsAdminOrReadOnly]
+#
+# class CommentListCreateView(generics.ListCreateAPIView):
+#     queryset = Comment.objects.all()
+#     serializer_class = CommentSerializer
+#     permission_classes = [IsAuthorOrAdminOrReadOnly]
+#
+#
+# class CommentRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Comment.objects.all()
+#     serializer_class = CommentSerializer
+#     permission_classes = [IsAuthorOrAdminOrReadOnly]
